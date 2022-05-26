2f6758428ccf5ec3812b312e32c0b09b42113c38
Cosmos Appiah <cosmosappiah029@gmail.com>
2022-05-26 22:15:44 +0000

    chore:added firebase

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZGU8tdqFko1frlpejt2TMqZg92QzdiCs",
+  authDomain: "lenda-c920c.firebaseapp.com",
+  projectId: "lenda-c920c",
+  storageBucket: "lenda-c920c.appspot.com",
+  messagingSenderId: "831808331011",
+  appId: "1:831808331011:web:660af0e652958618ac9d36",
+  measurementId: "G-M151GL9KYM",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
