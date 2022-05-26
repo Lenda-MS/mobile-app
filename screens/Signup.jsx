2852f7960d599de32532e3af9383c9daaf4d5e50
Cosmos Appiah <cosmosappiah029@gmail.com>
2022-05-26 23:19:19 +0000

    feat:added formik and yup integration

--- screens/Signup.jsx
+++ screens/Signup.jsx
@@ -13,10 +13,18 @@ import { Colors, FONTS } from "../theme";
 import { getScreenPercent } from "../utils";
 import { Button } from "../components";
 import { Screens } from "../navigations";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 export const Signup = ({ navigation }) => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
+  const signupSchema = Yup.object().shape({
+    fullname: Yup.string().required("Fullname is required"),
+    password: Yup.string()
+      .min(5, "Enter minimum of 5 characters")
+      .required("Password is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    phoneNumber: Yup.string().min(9, "Invalid phone number"),
+  });
 
   return (
     <KeyboardAwareScrollView
@@ -38,91 +46,128 @@ export const Signup = ({ navigation }) => {
             style={{ height: 80 }}
           />
         </View>
-        <View style={styles.formContainer}>
-          <View style={{ alignItems: "center" }}>
-            <Text
-              style={{
-                ...styles.label,
-                fontSize: getScreenPercent(10),
-                marginBottom: "10%",
-              }}
-            >
-              Signup
-            </Text>
-          </View>
-          <Text style={styles.label}>Full Name</Text>
-          <View style={styles.inputContainer}>
-            <View style={styles.input}>
-              <TextInput
-                style={{ flex: 1, borderWidth: 0 }}
-                autoFocus={true}
-                value={id}
-                onChangeText={(text) => setId(text)}
-              />
-            </View>
-            <Text style={styles.errorStyle}>Error</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Email</Text>
-            <View style={styles.input}>
-              <TextInput
-                style={{ flex: 1, borderWidth: 0 }}
-                autoFocus={true}
-                value={id}
-                onChangeText={(text) => setId(text)}
-              />
-            </View>
-            <Text style={styles.errorStyle}>Error</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Password</Text>
-            <View style={styles.input}>
-              <TextInput
-                style={{ flex: 1, borderWidth: 0 }}
-                autoFocus={true}
-                textContentType="password"
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-              />
-            </View>
-            <Text style={styles.errorStyle}>Error</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Phone Number</Text>
-            <View style={[styles.input, styles.phoneInput]}>
-              <Text
-                style={[styles.label, { marginRight: getScreenPercent(2) }]}
-              >
-                +233
-              </Text>
-              <TextInput
-                style={{ flex: 1, borderWidth: 0 }}
-                autoFocus={true}
-                textContentType="password"
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-              />
-            </View>
-            <Text style={styles.errorStyle}>Error</Text>
-          </View>
+        <Formik
+          initialValues={{
+            email: "",
+            fullname: "",
+            password: "",
+            phoneNumber: "",
+          }}
+          validationSchema={signupSchema}
+          onSubmit={(values, { resetForm }) => {
+            console.log(values);
+            resetForm();
+          }}
+        >
+          {({
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            errors,
+            values,
+            touched,
+          }) => (
+            <View style={styles.formContainer}>
+              <View style={{ alignItems: "center" }}>
+                <Text
+                  style={{
+                    ...styles.label,
+                    fontSize: getScreenPercent(10),
+                    marginBottom: "10%",
+                  }}
+                >
+                  Signup
+                </Text>
+              </View>
+              <Text style={styles.label}>Full Name</Text>
+              <View style={styles.inputContainer}>
+                <View style={styles.input}>
+                  <TextInput
+                    style={{ flex: 1, borderWidth: 0 }}
+                    onBlur={handleBlur("fullname")}
+                    value={values.fullname}
+                    onChangeText={handleChange("fullname")}
+                  />
+                </View>
+                {errors.fullname && touched.fullname ? (
+                  <Text style={styles.errorStyle}>{errors.fullname}</Text>
+                ) : null}
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>Email</Text>
+                <View style={styles.input}>
+                  <TextInput
+                    style={{ flex: 1, borderWidth: 0 }}
+                    keyboardType={"email-address"}
+                    value={values.email}
+                    onBlur={handleBlur("email")}
+                    onChangeText={handleChange("email")}
+                  />
+                </View>
+                {errors.email && touched.email ? (
+                  <Text style={styles.errorStyle}>{errors.email}</Text>
+                ) : null}
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>Password</Text>
+                <View style={styles.input}>
+                  <TextInput
+                    style={{ flex: 1, borderWidth: 0 }}
+                    textContentType="password"
+                    secureTextEntry={true}
+                    value={values.password}
+                    onBlur={handleBlur("password")}
+                    onChangeText={handleChange("password")}
+                  />
+                </View>
+                {errors.password && touched.password ? (
+                  <Text style={styles.errorStyle}>{errors.password}</Text>
+                ) : null}
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>Phone Number</Text>
+                <View style={[styles.input, styles.phoneInput]}>
+                  <Text
+                    style={[styles.label, { marginRight: getScreenPercent(2) }]}
+                  >
+                    +233
+                  </Text>
+                  <TextInput
+                    style={{ flex: 1, borderWidth: 0 }}
+                    value={values.phoneNumber}
+                    keyboardType={"phone-pad"}
+                    maxLength={9}
+                    onBlur={handleBlur("phoneNumber")}
+                    onChangeText={handleChange("phoneNumber")}
+                  />
+                </View>
+                {errors.phoneNumber && touched.phoneNumber ? (
+                  <Text style={styles.errorStyle}>{errors.phoneNumber}</Text>
+                ) : null}
+              </View>
 
-          <Button
-            title={"Signup"}
-            spinnerStyle={{ color: Colors.PRIMARY }}
-            textStyle={{ fontSize: 16, color: "white" }}
-            style={styles.button}
-          />
-          <View style={{ alignItems: "center", marginTop: "5%" }}>
-            <View style={{ flexDirection: "row" }}>
-              <Text style={styles.label}>Have an account?</Text>
-              <TouchableOpacity
-                onPress={() => navigation.navigate(Screens.LOGIN)}
-              >
-                <Text style={{ ...styles.label, marginLeft: 5 }}>Login</Text>
-              </TouchableOpacity>
+              <Button
+                title={"Signup"}
+                spinnerStyle={{ color: Colors.PRIMARY }}
+                textStyle={{ fontSize: 16, color: "white" }}
+                style={styles.button}
+                onPress={handleSubmit}
+              />
+              <View style={{ alignItems: "center", marginTop: "5%" }}>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.label}>Have an account?</Text>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate(Screens.LOGIN)}
+                  >
+                    <Text style={{ ...styles.label, marginLeft: 5 }}>
+                      Login
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
-          </View>
-        </View>
+          )}
+        </Formik>
       </SafeAreaView>
     </KeyboardAwareScrollView>
   );
