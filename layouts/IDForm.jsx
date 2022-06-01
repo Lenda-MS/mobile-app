b43f3cb74dfede6ed66fc16b8ac9da5884ff3b57
Cosmos Appiah <cosmosappiah029@gmail.com>
2022-06-01 00:19:02 +0000

    chore;set and preview image on pick

--- layouts/IDForm.jsx
+++ layouts/IDForm.jsx
@@ -5,6 +5,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import { Colors, FONTS } from "../theme";
 import { getScreenPercent } from "../utils";
@@ -12,9 +13,59 @@ import { Button } from "../components";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import GlassX, { useStore } from "glassx";
+import * as ImagePicker from "expo-image-picker";
+import { firebase } from "../firebase/config";
 
 export const IDForm = ({ useStep }) => {
   const [loading, setLoading] = useState();
+  const user = GlassX.get("user");
+
+  async function uploadImageAsync(uri, field) {
+    const fileExtension = uri.split(".").pop();
+
+    const fileName = `${user.id}-${field}-${new Date()
+      .toString()
+      .trim()
+      .replace(/ /g, "")}.${fileExtension}`;
+
+    // const blob = await new Promise((resolve, reject) => {
+    //   const xhr = new XMLHttpRequest();
+    //   xhr.onload = function () {
+    //     resolve(xhr.response);
+    //   };
+    //   xhr.onerror = function (e) {
+    //     console.log(e);
+    //     reject(new TypeError("Network request failed"));
+    //   };
+    //   xhr.responseType = "blob";
+    //   xhr.open("GET", uri, true);
+    //   xhr.send(null);
+    // });
+    // console.log(blob);
+    // const storageRef = firebase.storage().ref(`user.application/${fileName}`);
+
+    // await storageRef.put(blob);
+  }
+
+  const idSchema = Yup.object().shape({
+    idNumber: Yup.string().required("Ghana card number is required"),
+    idPicture: Yup.string().required("ID card picture is required"),
+    userPicture: Yup.string().required("Your picture is required"),
+  });
+
+  const pickImage = async (setImage, field) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(field, result.uri);
+      uploadImageAsync(result.uri, "idPicture");
+    }
+  };
 
   return (
     <View>
@@ -24,8 +75,11 @@ export const IDForm = ({ useStep }) => {
           idPicture: "",
           userPicture: "",
         }}
-        onSubmit={async (values, { resetForm }) => {}}
-        // validationSchema={}
+        validationSchema={idSchema}
+        onSubmit={async (values, { resetForm }) => {
+          console.log(values);
+          resetForm();
+        }}
       >
         {({
           handleBlur,
@@ -50,33 +104,56 @@ export const IDForm = ({ useStep }) => {
               <View style={styles.input}>
                 <TextInput
                   style={{ flex: 1, borderWidth: 0, ...styles.label }}
-                  value={values.address}
-                  onBlur={handleBlur("address")}
+                  value={values.idNumber}
+                  onBlur={handleBlur("idNumber")}
                   onChangeText={(text) => {
-                    handleChange("address")(text);
+                    handleChange("idNumber")(text);
                   }}
                 />
               </View>
-              {errors.address && touched.address ? (
-                <Text style={styles.errorStyle}>{errors.address}</Text>
+              {errors.idNumber && touched.idNumber ? (
+                <Text style={styles.errorStyle}>{errors.idNumber}</Text>
               ) : null}
             </View>
             <View style={styles.inputContainer}>
-              <Text style={styles.label}>Upload ID card</Text>
+              <Text style={styles.label}>ID card</Text>
               <TouchableOpacity
-                style={{ ...styles.input, ...styles.imagePicker }}
-              ></TouchableOpacity>
-              {errors.address && touched.address ? (
-                <Text style={styles.errorStyle}>{errors.address}</Text>
+                style={{ ...styles.imagePicker }}
+                onPress={() => pickImage(setFieldValue, "idPicture")}
+                onBlur={handleBlur("idPicture")}
+              >
+                {values.idPicture ? (
+                  <Image
+                    resizeMode="stretch"
+                    style={styles.imagePreview}
+                    source={{ uri: values.idPicture }}
+                  />
+                ) : (
+                  <UploadNotice text={"Upload your ID card"} />
+                )}
+              </TouchableOpacity>
+              {errors.idPicture && touched.idPicture ? (
+                <Text style={styles.errorStyle}>{errors.idPicture}</Text>
               ) : null}
             </View>
             <View style={styles.inputContainer}>
-              <Text style={styles.label}>Upload your picture</Text>
+              <Text style={styles.label}>Your picture</Text>
               <TouchableOpacity
-                style={{ ...styles.input, ...styles.imagePicker }}
-              ></TouchableOpacity>
-              {errors.address && touched.address ? (
-                <Text style={styles.errorStyle}>{errors.address}</Text>
+                style={{ ...styles.imagePicker }}
+                onPress={() => pickImage(setFieldValue, "userPicture")}
+              >
+                {values.userPicture ? (
+                  <Image
+                    resizeMode="stretch"
+                    style={styles.imagePreview}
+                    source={{ uri: values.userPicture }}
+                  />
+                ) : (
+                  <UploadNotice text={"Upload your picture"} />
+                )}
+              </TouchableOpacity>
+              {errors.userPicture && touched.userPicture ? (
+                <Text style={styles.errorStyle}>{errors.userPicture}</Text>
               ) : null}
             </View>
 
@@ -103,6 +180,29 @@ export const IDForm = ({ useStep }) => {
   );
 };
 
+const UploadNotice = ({ text }) => {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Image
+        resizeMode="contain"
+        style={{
+          height: getScreenPercent(20),
+          marginBottom: getScreenPercent(3),
+        }}
+        source={require("../assets/upload.png")}
+      />
+      <Text
+        style={{
+          fontFamily: FONTS.WORK_SANS_MEDIUM,
+          fontSize: getScreenPercent(4.5),
+        }}
+      >
+        {text}
+      </Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   formContainer: {
     paddingHorizontal: "8%",
@@ -111,6 +211,7 @@ const styles = StyleSheet.create({
     fontFamily: FONTS.WORK_SANS_MEDIUM,
     fontSize: 16,
   },
+  imagePreview: { width: "100%", height: "100%", borderRadius: 8 },
   button: {
     borderColor: Colors.SECONDARY,
     width: "47%",
@@ -139,6 +240,12 @@ const styles = StyleSheet.create({
   },
   imagePicker: {
     height: getScreenPercent(50),
+    borderBottomColor: Colors.SECONDARY,
+    borderWidth: 1.5,
+    borderRadius: 10,
+    marginTop: "2%",
+    marginBottom: 2,
+    padding: 2,
   },
 
   input: {
